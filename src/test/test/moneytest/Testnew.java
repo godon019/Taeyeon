146b571cbfef8c57aa0e6c9f8afb2f6f194550e6
godon019 <godon019@gmail.com>
2016-07-12 08:09:40 +0900

    test

--- src/test/test/moneytest/Testnew.java
+++ src/test/test/moneytest/Testnew.java
@@ -29,7 +29,6 @@ public class Testnew {
     public void testCurrency(){
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-
     }
 
     @Test
